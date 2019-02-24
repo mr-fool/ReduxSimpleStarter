71b58952412388f721230e238b07cb1b2adf2f16
mr-fool <mr-fool@users.noreply.github.com>
2019-02-23 19:09:58 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import React from 'react';
+
+
 //Create a new component. This should prodouce
 //some html
 const App = function() {
-    return <div>hi!</div>
+    return <div>hi!</div>;
 }
 
 //Take this component's generated html and put it 
 //on the page (in the dom)
-
+React.render(App);
