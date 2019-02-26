073f6cec8f6f2ad47669eb4a099af28c907d0879
mr-fool <mr-fool@users.noreply.github.com>
2019-02-25 20:12:25 -0700

    fixed a typo

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {video: []};
+        this.state = {videos: []};
         YTSearch({key: API_KEY,term: 'surfboards'}, (videos) =>{
             this.setState({videos});
         });
