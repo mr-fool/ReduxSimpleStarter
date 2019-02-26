c37000c953c7ba832daac0c013199c2c7a15a57d
mr-fool <mr-fool@users.noreply.github.com>
2019-02-25 18:57:12 -0700

    componentize index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+
 
 const API_KEY = 'AIzaSyDCqCdFznaXXevzggHSzrR7oRmt4o1R5YA';
 
+
+YTSearch({key: API_KEY,term: 'surfboards'}, function(data) {
+    console.log(data);
+});
+
+
 //Create a new component. This should prodouce
 //some html
-const App = () => {
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {video: []};
+    }
+
+
+    render() {
     return (<div>
 
     <SearchBar / >
 
     </div>
     );
+    }
 }
 
 //Take this component's generated html and put it 
