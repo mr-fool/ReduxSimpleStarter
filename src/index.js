b023604ced60eec2da8a4f88f7bb7e2fa141eee5
mr-fool <mr-fool@users.noreply.github.com>
2019-02-28 14:08:27 -0700

    search bar now works

--- src/index.js
+++ src/index.js
@@ -21,7 +21,11 @@ class App extends Component {
             videos: [],
             selectedVideo: null
         };
-        YTSearch({key: API_KEY,term: 'surfboards'}, (videos) =>{
+        this.videoSearch('meme');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY,term: term}, (videos) =>{
             this.setState({
                 videos: videos,
                 selectedVideo: videos[0]
@@ -30,11 +34,12 @@ class App extends Component {
     }
 
 
+
     render() {
     return (
         <div>
 
-            <SearchBar / >
+            <SearchBar onSearchTermChange={term => this.videoSearch(term)} / >
             <VideoDetail video={this.state.selectedVideo}/>
             <VideoList
                 onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
