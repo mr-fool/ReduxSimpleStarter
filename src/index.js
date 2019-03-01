758b2a72e5ea2da64446acae4fbbdb46a426a566
mr-fool <mr-fool@users.noreply.github.com>
2019-02-28 22:10:44 -0700

    delay function

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import YTSearch from 'youtube-api-search';
 import VideoDetail from "./components/video_details";
+import _ from 'lodash';
+
 
 const API_KEY = 'AIzaSyDCqCdFznaXXevzggHSzrR7oRmt4o1R5YA';
 
@@ -36,10 +38,13 @@ class App extends Component {
 
 
     render() {
+
+        const videoSearch = _.debounce((term)=> {this.videoSearch(term)}, 300);
+
     return (
         <div>
 
-            <SearchBar onSearchTermChange={term => this.videoSearch(term)} / >
+            <SearchBar onSearchTermChange={videoSearch} / >
             <VideoDetail video={this.state.selectedVideo}/>
             <VideoList
                 onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
